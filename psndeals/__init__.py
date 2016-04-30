6d95a2e4d711f580081b3b219d142c0419a84d1e
Dmitry Kireev <dmitry@kireev.co>
2016-04-29 23:44:00 -0700

    Get deal stores, add sort params

--- psndeals/__init__.py
+++ psndeals/__init__.py
@@ -35,18 +35,29 @@ def cli(ctx, config, verbose):
 
 @cli.command()
 @click.option('--platform', '-p', default='ps4')
+@click.option('--country', '-c', default='US')
+@click.option('--sort', '-s', type=click.Choice(['savings', 'price']), default='savings')
+@click.option('--reverse-sort', '-r', is_flag=True, default=True)
 @pass_psndeals
-def show(psndeals, platform):
-
-    deals = get_deals(platform)
+def show(psndeals, platform, country, sort, reverse_sort):
+    deals = get_deals(platform, country)
     if deals:
         # Sort
-        deals.sort(key=operator.itemgetter('savings'), reverse=True)
+        deals.sort(key=operator.itemgetter(sort), reverse=reverse_sort)
         for i, deal in enumerate(deals):
             if deal['savings'] > 15 or (deal['savings'] > 10 and deal['discount'] == 100):
                 click.secho(u"------------------------------------------------------")
-                click.secho(u"{name}".format(name=deal['name']), fg='green')
-                click.secho(u"{release_date}. Original price: ${orig_price}. Savings: ${savings}".format(name=deal['name'], discount=deal['discount'], release_date=time.strftime("%b %Y", deal['release_date']), orig_price=deal['orig_price'], savings=deal['savings']))
+
+                click.secho(u"{name} ({release_date})".format(name=deal['name'], release_date=time.strftime("%b %Y", deal['release_date'])), fg='blue')
+                # click.secho(u"Price: ${price}.\nOriginal price: ${orig_price}.\nSavings: ${savings}".format(price=deal['price'],name=deal['name'], discount=deal['discount'], orig_price=deal['orig_price'], savings=deal['savings']))
+
+                if deal['price'] == 0:
+                    click.secho(u"Price: {price}".format(price="Free"), fg='green')
+                else:
+                    click.secho(u"Price: ${price}".format(price=deal['price']))
+                click.secho(u"Original price: ${orig_price}.".format(orig_price=deal['orig_price']))
+                click.secho(u"Savings: ${savings}".format(savings=deal['savings']))
+
                 # print(i)
                 # print(len(deals)-1)
                 if i == len(deals)-1:
@@ -76,11 +87,38 @@ def authorize():
     # signInInput_SignInID
     # signInInput_Password
     pass
-def get_deals(platform):
-    urls = [
-        'https://store.sonyentertainmentnetwork.com/store/api/chihiro/00_09_000/container/US/en/999/STORE-MSF77008-ALLDEALS?platform~ps4',
-        'https://store.playstation.com/chihiro-api/viewfinder/US/en/19/STORE-MSF77008-PSPLUSFREEGAMES?platform~ps4'
+
+def get_store_urls(platform, country):    
+    request_data = '{}'
+    store_urls = []
+
+    # Load Weekly Deals
+    weekly_deals_url = 'https://store.playstation.com/store/api/chihiro/00_09_000/container/US/en/999/STORE-MSF77008-WEEKLYDEALS'
+    response = requests.get(weekly_deals_url, data=request_data)
+    
+    if response.ok:
+        response_data = json.loads(response.content)
+        for link in response_data['links']:            
+            if 'url' in link:
+                store_urls.append(link['url'])
+        
+    additional_stores = [
+        'STORE-MSF77008-PSPLUSDISCOUNTS',
+        'STORE-MSF77008-PSPLUSFREEGAMES',
     ]
+
+    for additional_store in additional_stores:
+        store_urls.append("https://store.sonyentertainmentnetwork.com/store/api/chihiro/00_09_000/container/US/en/999/{additional_store}".format(additional_store=additional_store, platform=platform))
+    
+    # Add platform and country filters
+    for key, store_url in enumerate(store_urls):
+        store_urls[key] = store_url + "?platform={platform}&country={country}".format(platform=platform, country=country)
+
+    return store_urls
+    
+def get_deals(platform, country):    
+    urls = get_store_urls(platform, country)
+    # exit(0)
 
     deals = []
     for url in urls:
@@ -88,17 +126,18 @@ def get_deals(platform):
         response = requests.get(url, data=request_data)
 
         if response.ok:
-
             response_data = json.loads(response.content)
             for item in response_data['links']:
-                deals.append({
-                    'psn_sku_id': item['id'],
-                    'name': item['name'],
-                    'discount': item['default_sku']['rewards'][0]['discount'],
-                    'orig_price': (item['default_sku']['price'] / 100),
-                    'savings': round((item['default_sku']['price'] / 100 * item['default_sku']['rewards'][0]['discount'] / 100),2),
-                    'release_date': time.strptime(item['release_date'], "%Y-%m-%dT%H:%M:%SZ") # 2015-12-05T00:00:00Z
-                })
+                if 'default_sku' in item:
+                    deals.append({
+                        'psn_sku_id': item['id'],
+                        'name': item['name'],
+                        'discount': item['default_sku']['rewards'][0]['discount'],
+                        'orig_price': (item['default_sku']['price'] / 100),
+                        'price': (item['default_sku']['rewards'][0]['price'] / 100),
+                        'savings': round((item['default_sku']['price'] / 100 * item['default_sku']['rewards'][0]['discount'] / 100),2),
+                        'release_date': time.strptime(item['release_date'], "%Y-%m-%dT%H:%M:%SZ") # 2015-12-05T00:00:00Z
+                    })
         else:
             # If response code is not ok (200), print the resulting http error code with description
             response.raise_for_status()
